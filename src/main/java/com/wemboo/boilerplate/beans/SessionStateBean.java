458cdc84fe46cceeef2c3723dd91a7a836f733c5
osh <oshishkin@gmail.com>
2018-04-27 12:56:02 +0200

    + Show database table with Primefaces

--- src/main/java/com/wemboo/boilerplate/beans/SessionStateBean.java
+++ src/main/java/com/wemboo/boilerplate/beans/SessionStateBean.java
@@ -4,24 +4,43 @@
 package com.wemboo.boilerplate.beans;
 
 import java.io.Serializable;
-
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+import com.wemboo.boilerplate.db.SampleEntity;
+import com.wemboo.boilerplate.db.SampleRepository;
+import org.slf4j.Logger;
 
 @SessionScoped
+@ManagedBean(name="sampleStateBean")
 public class SessionStateBean implements Serializable{
 
     public static final String GREETING_TEXT = "Hello World!";
     private String greeting;
 
+    private List<SampleEntity> result;
+
+    @Inject
+    private SampleRepository SampleRepository;
+
+    @Inject
+    private Logger log;    
 
     @PostConstruct
     public void init() {
-      greeting = GREETING_TEXT;
+        log.debug("Initialize bean");
+        greeting = GREETING_TEXT;
+        result = SampleRepository.findAll();      
     }
 
     public String getGreeting() {
         return greeting;
+    }
+
+    public List<SampleEntity> getResult() {
+        return result;
     }
 
 }
